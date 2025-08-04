5cacf26e54c610d2b179f74d10ea2aed2927e06e
Ubuntu <ubuntu@ip-172-31-23-88.us-east-2.compute.internal>
2025-08-04 21:09:11 +0000

    updated placeholders

--- bin/exchange.ml
+++ bin/exchange.ml
@@ -22,7 +22,7 @@ module Orders = struct
   [@@deriving fields]
 end
 
-let red_orders =
+let red_orders (red_bid: int option) (red_ask: int option) =
   Vdom.Node.div
     ~attrs:[ Vdom.Attr.classes [ "red" ] ]
     [ Vdom.Node.img
@@ -32,14 +32,14 @@ let red_orders =
         ~attrs:[ Vdom.Attr.classes [ "orders" ] ]
         [ Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Bid"
+              [ Vdom.Attr.placeholder (match red_bid with |Some bid -> (Int.to_string bid) |None -> "Bid")
               ; Vdom.Attr.classes [ "red_order" ]
               ; Vdom.Attr.type_ "number"
               ]
             ()
         ; Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Ask"
+              [ Vdom.Attr.placeholder (match red_ask with |Some ask -> Int.to_string ask |None -> "Ask")
               ; Vdom.Attr.classes [ "red_order" ]
               ; Vdom.Attr.type_ "number"
               ]
@@ -48,7 +48,7 @@ let red_orders =
     ]
 ;;
 
-let yellow_orders : Vdom.Node.t =
+let yellow_orders (yellow_bid: int option) (yellow_ask: int option) =
   Vdom.Node.div
     ~attrs:[ Vdom.Attr.classes [ "yellow" ] ]
     [ Vdom.Node.img
@@ -58,14 +58,14 @@ let yellow_orders : Vdom.Node.t =
         ~attrs:[ Vdom.Attr.classes [ "orders" ] ]
         [ Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Bid"
+              [ Vdom.Attr.placeholder (match yellow_bid with |Some bid -> Int.to_string bid |None -> "Bid")
               ; Vdom.Attr.classes [ "yellow_order" ]
               ; Vdom.Attr.type_ "number"
               ]
             ()
         ; Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Ask"
+              [ Vdom.Attr.placeholder (match yellow_ask with |Some ask -> Int.to_string ask |None -> "Ask")
               ; Vdom.Attr.classes [ "yellow_order" ]
               ; Vdom.Attr.type_ "number"
               ]
@@ -74,7 +74,7 @@ let yellow_orders : Vdom.Node.t =
     ]
 ;;
 
-let blue_orders : Vdom.Node.t =
+let blue_orders (blue_bid: int option) (blue_ask: int option) =
   Vdom.Node.div
     ~attrs:[ Vdom.Attr.classes [ "blue" ] ]
     [ Vdom.Node.img
@@ -84,14 +84,14 @@ let blue_orders : Vdom.Node.t =
         ~attrs:[ Vdom.Attr.classes [ "orders" ] ]
         [ Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Bid"
+              [ Vdom.Attr.placeholder (match blue_bid with |Some bid -> Int.to_string bid |None -> "Bid")
               ; Vdom.Attr.classes [ "blue_order" ]
               ; Vdom.Attr.type_ "number"
               ]
             ()
         ; Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Ask"
+              [ Vdom.Attr.placeholder (match blue_ask with |Some ask -> Int.to_string ask |None -> "Ask")
               ; Vdom.Attr.classes [ "blue_order" ]
               ; Vdom.Attr.type_ "number"
               ]
@@ -100,7 +100,7 @@ let blue_orders : Vdom.Node.t =
     ]
 ;;
 
-let green_orders : Vdom.Node.t =
+let green_orders (green_bid: int option) (green_ask: int option) =
   Vdom.Node.div
     ~attrs:[ Vdom.Attr.classes [ "green" ] ]
     [ Vdom.Node.img
@@ -110,14 +110,14 @@ let green_orders : Vdom.Node.t =
         ~attrs:[ Vdom.Attr.classes [ "orders" ] ]
         [ Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Bid"
+              [ Vdom.Attr.placeholder (match green_bid with |Some bid -> Int.to_string bid |None -> "Bid")
               ; Vdom.Attr.classes [ "green_order" ]
               ; Vdom.Attr.type_ "number"
               ]
             ()
         ; Vdom.Node.input
             ~attrs:
-              [ Vdom.Attr.placeholder "Ask"
+              [ Vdom.Attr.placeholder (match green_ask with |Some ask -> Int.to_string ask |None -> "Ask")
               ; Vdom.Attr.classes [ "green_order" ]
               ; Vdom.Attr.type_ "number"
               ]
@@ -177,17 +177,17 @@ let submit_button ~player_id ~on_submit : Vdom.Node.t =
 
 (* Bonsai component *)
 
-let component ~player_id =
+let component ~player_id red_bid red_ask blue_bid blue_ask yellow_bid yellow_ask green_bid green_ask=
   Vdom.Node.div
     [ Vdom.Node.form
         ~attrs:[ Vdom.Attr.classes [ "exchange_page" ] ]
-        [ red_orders
-        ; yellow_orders
-        ; blue_orders
-        ; green_orders
+        [ red_orders red_bid red_ask
+        ; yellow_orders yellow_bid yellow_ask
+        ; blue_orders blue_bid blue_ask
+        ; green_orders green_bid green_ask
         ; submit_button ~player_id ~on_submit:(fun _ -> Ui_effect.Ignore)
         ]
     ]
 ;;
 
-let serve_body ~player_id = component ~player_id
+let serve_body ~player_id red_bid red_ask blue_bid blue_ask yellow_bid yellow_ask green_bid green_ask = component ~player_id red_bid red_ask blue_bid blue_ask yellow_bid yellow_ask green_bid green_ask
--- bin/main.ml
+++ bin/main.ml
@@ -42,7 +42,7 @@ let dummy_client_state : Client_state.t Bonsai.t =
     ; my_red_bid = None
     ; my_yellow_bid = None
     ; my_blue_bid = None
-    ; my_green_bid = None
+    ; my_green_bid = Some 5
     ; my_red_ask = None
     ; my_yellow_ask = None
     ; my_blue_ask = None
@@ -55,18 +55,34 @@ let page
   (client_state : Client_state.t Bonsai.t)
   (local_ _graph)
   =
-  let%sub {current_phase;me;all_trades;_} =
+  let%sub {current_phase;me;all_trades;
+   my_red_bid 
+    ; my_yellow_bid 
+    ; my_blue_bid 
+    ; my_green_bid 
+    ; my_red_ask 
+    ; my_yellow_ask 
+    ; my_blue_ask 
+    ; my_green_ask
+    ; _} =
     client_state
   in
   let%sub {id;cash;_} = me
   in
   match%sub current_phase with
   | Current_phase.Playing ->
-    let%arr id and all_trades and cash in
+    let%arr id and all_trades and cash and my_red_bid and my_green_ask and my_blue_ask and my_blue_bid and my_green_bid and my_red_ask and my_yellow_ask and my_yellow_bid in
     Vdom.Node.div
       ~attrs:[ Vdom.Attr.classes [ "full_page" ] ]
       [ body cash
-      ; Exchange.serve_body ~player_id:id
+      ; Exchange.serve_body ~player_id:id my_red_bid 
+    my_yellow_bid 
+    my_blue_bid 
+    my_green_bid 
+     my_red_ask 
+    my_yellow_ask 
+    my_blue_ask 
+    my_green_ask
       ; Trade_history.body all_trades ]
   | _ -> Bonsai.return {%html|<p>Waiting for others...</p>|}
 ;;
