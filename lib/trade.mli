ad66c62dbab63257c7f2b38fa9495c9efc6ea8dd
Ubuntu <josephor@mit.edu>
2025-08-08 15:33:02 +0000

    .

--- lib/trade.mli
+++ lib/trade.mli
@@ -2,6 +2,5 @@ open! Core
 
 val get_best_bid_order : bid_order_list:Order.t list -> (int * string) option
 val get_best_ask_order : ask_order_list:Order.t list -> (int * string) option
-val match_orders : Game_state.t -> Game_state.t
 
 
