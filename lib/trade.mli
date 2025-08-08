381a1f826425256018bd21803cf2adcf8e8eb2bd
Ubuntu <josephor@mit.edu>
2025-08-08 15:32:47 +0000

    .

--- lib/trade.mli
+++ lib/trade.mli
@@ -4,6 +4,4 @@ val get_best_bid_order : bid_order_list:Order.t list -> (int * string) option
 val get_best_ask_order : ask_order_list:Order.t list -> (int * string) option
 val match_orders : Game_state.t -> Game_state.t
 
-val get_best_bids_and_asks
-  :  Game_state.t
-  -> (Racer.t, string * string, Racer.comparator_witness) Map_intf.Map.t
+
