fb892bb5cb9d58a5c1fa2b37d9070837288547b0
Preston Williams <prestonwilliams987@gmail.com>
2025-08-08 15:37:00 +0000

    working on fix

--- lib/trade.mli
+++ lib/trade.mli
@@ -2,4 +2,4 @@ open! Core
 
 val get_best_bid_order : bid_order_list:Order.t list -> (int * string) option
 val get_best_ask_order : ask_order_list:Order.t list -> (int * string) option
-
+val match_orders : Game_state.t -> Game_state.t
